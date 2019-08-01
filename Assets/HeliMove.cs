4affbb67bdfa2542e60988ca00e2ea69bc726032
Avi Marks <alanmarks@Avis-MacBook-Pro.local>
2019-07-31 22:00:46 -0500

    movement w/ roll and rotation fully working. this is tight.

--- Assets/HeliMove.cs
+++ Assets/HeliMove.cs
@@ -7,10 +7,13 @@ public class HeliMove : MonoBehaviour
     GameObject moveHere;
     public GameObject testMovePoint;
     public GameObject arMovePoint;
+    public GameObject arCam;
 
     float time;
     Vector2 horizontalRefVel = Vector3.zero;
     float vertRefVel = 0f;
+
+    public float rollSpeed = .2f;
 
     public bool useTestMovePoint;
 
@@ -31,6 +34,8 @@ public class HeliMove : MonoBehaviour
         if (time > 3f){
             HorizontalMove();
             VerticalMove();
+            MoveRotation();
+            RollRotation();
         }
         
     }
@@ -48,5 +53,34 @@ public class HeliMove : MonoBehaviour
         var newVertPos = Mathf.SmoothDamp(thisVertPos, targetVertPos, ref vertRefVel, .7f);
         Vector3 newVector3Pos = new Vector3(transform.position.x, newVertPos, transform.position.z);
         transform.position = newVector3Pos;
+    }
+
+    void MoveRotation(){
+        Quaternion thisRot = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
+        Quaternion camRot = Quaternion.Euler(0f, arCam.transform.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Slerp(thisRot, camRot, .05f);
+        
+    }
+
+    void RollRotation(){
+        //giving new variable so we can mess with it variably every frame based on how far target Pose is 
+        float rollSlerpValue = rollSpeed;
+
+        //how close the target position has to be for the roll amount to start scaling
+        float rollCutoff = 2f;
+
+        //we use vector3 positions for calculating distance with no y value because otherwise this distance scaling method of calculating roll causes 
+        //problems with vertical movement
+        var flatTargetPos = new Vector3(moveHere.transform.position.x, 0f, moveHere.transform.position.z);
+        var flatThisPos = new Vector3(this.transform.position.x, 0f, this.transform.position.z);
+        var distance = Vector3.Distance(flatTargetPos, flatThisPos);
+        //calculating whether or not roll needs to be scaled/scaling if necessary
+        if (distance < rollCutoff){
+            rollSlerpValue = rollSlerpValue * (distance/rollCutoff);
+        }
+
+        var direction = (moveHere.transform.position - this.transform.position).normalized;
+        var newDirection = Quaternion.Slerp(this.transform.rotation, Quaternion.FromToRotation(Vector3.up, direction), rollSlerpValue);
+        transform.rotation = Quaternion.Euler(newDirection.eulerAngles.x, transform.eulerAngles.y, newDirection.eulerAngles.z);
     }
 }
