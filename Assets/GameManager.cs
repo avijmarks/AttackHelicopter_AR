5eb9ba302340b091ca5c766a21cd92f2885cc9c1
Avi Marks <avijmarks@g.ucla.edu>
2019-11-15 11:22:33 -0600

    added gamemanager and 30sec unpaid version countdown with b/w screen effect afterwards

---
+++ Assets/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; 
+    public bool paidVersion = false;
+
+    [SerializeField]
+    protected Text countDownText;
+    [SerializeField]
+    protected GameObject unpaidVersionPanel;
+    [SerializeField]
+    protected PostProcessVolume blackAndWhiteEffect;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //singleton code :)
+        if (instance == null){
+            instance = this; 
+        } else if (instance != this){
+            Destroy(gameObject);
+        }
+    }
+
+    void Start ()
+    {
+        InitialCheckPaidVersion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void InitialCheckPaidVersion()
+    {
+        if (!paidVersion){
+            CountDown();
+        } else if (paidVersion){
+            unpaidVersionPanel.SetActive(false);
+        }
+        
+    }
+
+    public void CountDown ()
+    {
+        StartCoroutine("ICountDown");
+       
+    }
+    IEnumerator ICountDown ()
+    {
+        float time = 30f;
+        string currentTimeString;
+        while (time > 0){
+            time -= Time.deltaTime;
+            currentTimeString = Mathf.RoundToInt(time).ToString();
+            countDownText.text = currentTimeString;
+            yield return null;
+        }
+
+        BlackWhiteScreen();   
+    }
+    void BlackWhiteScreen (){
+        ColorGrading colorGradingLayer;
+        blackAndWhiteEffect.profile.TryGetSettings(out colorGradingLayer);
+        colorGradingLayer.enabled.value = true;
+    }
+    
+    
+}
