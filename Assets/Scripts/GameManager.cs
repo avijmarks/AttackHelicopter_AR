b96907f006026eed0045d1f6ebabcb6c5dbf2d1f
Avi Marks <avijmarks@g.ucla.edu>
2019-11-26 15:42:02 -0600

    IAP panel and banner logic working correctly, interfaces with settings properly and is own script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Rendering.PostProcessing;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,14 +9,10 @@ public class GameManager : MonoBehaviour
     public bool paidVersion = false;
 
     [SerializeField]
-    protected Text countDownText;
-    [SerializeField]
-    protected GameObject unpaidVersionPanel;
-    [SerializeField]
-    protected PostProcessVolume blackAndWhiteEffect;
-    // Start is called before the first frame update
-    public GameObject unpaidWaitPanel;
-    public Text unpaidWaitCountdownText;
+    protected IAPDelayWindowManager iAPDelayWindowManager;
+    
+
+    
     void Awake()
     {
         //singleton code :)
@@ -30,7 +25,7 @@ public class GameManager : MonoBehaviour
 
     void Start ()
     {
-        InitialCheckPaidVersion();
+       
     }
 
     // Update is called once per frame
@@ -38,62 +33,15 @@ public class GameManager : MonoBehaviour
     {
         
     }
-    void InitialCheckPaidVersion()
-    {
-        if (!paidVersion){
-            CountDown();
-        } else if (paidVersion){
-            unpaidVersionPanel.SetActive(false);
-        }   
-    }
-
-    public void CountDown ()
-    {
-        StartCoroutine("IInitialCountDown");
-    }
-
-    IEnumerator IInitialCountDown ()
-    {
-        float time = 10f;
-        string currentTimeString;
-        while (time > 0){
-            time -= Time.deltaTime;
-            currentTimeString = Mathf.RoundToInt(time).ToString();
-            countDownText.text = currentTimeString;
-            yield return null;
-        }
-
-        ShowUnpaidPanel();  
-    }
-
-    void ShowUnpaidPanel()
-    {
-        StartCoroutine("IWaitPanelCountdown");
-        Debug.Log("when is this called");
-    }
-
-    IEnumerator IWaitPanelCountdown ()
-    {
-        unpaidWaitPanel.SetActive(true);
-        float time = 10f;
-        string currentTimeString;
-        while (time > 0){
-            time -= Time.deltaTime;
-            currentTimeString = Mathf.RoundToInt(time).ToString();
-            unpaidWaitCountdownText.text = currentTimeString;
-            yield return null;
-        }
-
-        unpaidWaitPanel.SetActive(false);
-    }
+    
 
 
     //old function for b/w screen effect, DELETE AND DELETE ALL POSTPROCESSING STUFF FROM ASSETS
-    void BlackWhiteScreen (){
-        ColorGrading colorGradingLayer;
-        blackAndWhiteEffect.profile.TryGetSettings(out colorGradingLayer);
-        colorGradingLayer.enabled.value = true;
-    }
+    // void BlackWhiteScreen (){
+    //     ColorGrading colorGradingLayer;
+    //     blackAndWhiteEffect.profile.TryGetSettings(out colorGradingLayer);
+    //     colorGradingLayer.enabled.value = true;
+    // }
     
     
 }
---
+++ Assets/Scripts/IAPDelayWindowManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IAPDelayWindowManager : MonoBehaviour
+{   
+    [SerializeField]
+    protected GameObject unpaidBanner;
+    [SerializeField]
+    protected Text countDownText;
+    [SerializeField]
+    protected GameObject unpaidWaitPanel;
+    [SerializeField]
+    protected Text unpaidWaitCountdownText;
+
+    bool bannerCountDownActive = false;
+    bool panelCountDownActive = false;
+    IEnumerator coroutineToStart;
+    int promptDisplayCount = 0;
+    float initialBannerCountTime = 30f;
+    float normalBannerCountTime = 60f;
+    float panelDisplayTime = 10f;
+
+    [SerializeField]
+    protected GameObject playerSettings;
+
+
+    //ALL THIS LOGIC FUNCTIONS AS A BIG LOOP THAT RUNS 2 COROUTINES(ONE FOR BANNER ONE FOR PANEL COUNTDOWNS)
+    //CONSTANTLY CHECKS PAID VERSION BOOLEAN IN GAMEMANAGER.CS AND CAN STOP ANY TIME THATS CHANGE (CHECK IN UPDATE)
+    //CAN CHANGE INITIAL BANNER COUNTDOWN, THE NORMAL COUNTDOWN, AND THE PANEL DISPLAY COUNTDOWN IN VARIABLES ABOVE
+    
+    void Update()
+    {
+        if (GameManager.instance.paidVersion == true){
+            StopAllCoroutines();
+            DisableAllIAPPrompts();
+        }
+
+        if (!bannerCountDownActive && !panelCountDownActive && !GameManager.instance.paidVersion){
+            StartCountdownCycle();
+        }
+
+        
+    }
+
+    void DisableAllIAPPrompts(){
+        unpaidBanner.SetActive(false);
+        unpaidWaitPanel.SetActive(false);
+    }
+
+    void StartCountdownCycle (){
+        bannerCountDownActive = true;
+        float bannerCountTime = promptDisplayCount > 0 ? normalBannerCountTime : initialBannerCountTime;
+        promptDisplayCount += 1;
+        BannerCountDown(bannerCountTime);
+    }
+    
+    public void BannerCountDown (float initialCountTime)
+    {
+        coroutineToStart = IBannerCountDown(initialCountTime);
+        StartCoroutine(coroutineToStart);
+    }
+
+    IEnumerator IBannerCountDown (float countTime)
+    {
+        float time = countTime;
+        string currentTimeString;
+        while (time > 0){
+            float timePassed = playerSettings.activeSelf ? 0f : Time.deltaTime;
+            time -= timePassed;
+            currentTimeString = Mathf.RoundToInt(time).ToString();
+            countDownText.text = currentTimeString;
+            yield return null;
+        }
+        bannerCountDownActive = false;
+        ShowUnpaidPanel();  
+    }
+
+    void ShowUnpaidPanel()
+    {
+        panelCountDownActive = true;
+        coroutineToStart = IWaitPanelCountdown(panelDisplayTime);
+        StartCoroutine(coroutineToStart);
+        Debug.Log("when is this called");
+    }
+
+    IEnumerator IWaitPanelCountdown (float countTime)
+    {
+        unpaidWaitPanel.SetActive(true);
+        float time = countTime;
+        string currentTimeString;
+        while (time > 0){
+            float timePassed = playerSettings.activeSelf ? 0f : Time.deltaTime;
+            time -= timePassed;
+            currentTimeString = Mathf.RoundToInt(time).ToString();
+            unpaidWaitCountdownText.text = currentTimeString;
+            yield return null;
+        }
+        panelCountDownActive = false;
+        unpaidWaitPanel.SetActive(false);
+    }
+}
